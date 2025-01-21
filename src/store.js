b83577c95a3d27843fa44dee1c13f31e3cb9946a
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-21 22:09:44 +0100

    add Redux Saga middleware to store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./features/movies/moviesSlice";
+import createSagaMiddleware from "redux-saga";
+import { watchFetchMovies } from "./features/movies/moviesSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movies: moviesReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(watchFetchMovies);
 
 export default store;
