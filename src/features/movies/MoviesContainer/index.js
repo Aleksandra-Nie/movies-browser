ef29018f2e7f62a464f4e184f9c4bc2c959c2e0f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-21 01:24:14 +0100

    create MoviesContainer component and styled components (StyledMoviesContainer, Header)

---
+++ src/features/movies/MoviesContainer/index.js
@@ -0,0 +1,9 @@
+import { StyledMoviesContainer, Header } from "./styled";
+
+const MoviesContainer = () => (
+    <StyledMoviesContainer>
+        <Header>Popular movies</Header>
+    </StyledMoviesContainer>
+);
+
+export default MoviesContainer;
---
+++ src/features/movies/MoviesContainer/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledMoviesContainer = styled.div`
+    max-width: 1368px;
+    margin: 56px auto 0 auto;
+`;
+
+export const Header = styled.h2`
+    font-size: 36px;
+    font-weight: 600;
+`;
