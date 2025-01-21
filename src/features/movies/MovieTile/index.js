d7c6261584c73c9686218507297e374b0db06352
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-21 21:26:41 +0100

    add fetchMovies function

--- src/features/movies/MovieTile/index.js
+++ src/features/movies/MovieTile/index.js
@@ -12,24 +12,26 @@ import {
     Votes,
 } from "./styled";
 
-const MovieTile = () => (
-    <Tile>
-        <MoviePoster src={exampleMoviePoster} alt="movie poster" />
-        <div>
-            <MovieTileHeader>Mulan</MovieTileHeader>
-            <MovieTileYear>2020</MovieTileYear>
-            <GenresList>
-                <Genres>Action</Genres>
-                <Genres>Adventure</Genres>
-                <Genres>Drama</Genres>
-            </GenresList>
-            <MovieRating>
-                <StyledStarIcon />
-                <Rating>7,8</Rating>
-                <Votes>35 votes</Votes>
-            </MovieRating>
-        </div>
-    </Tile>
-);
+const MovieTile = () => {
+    return (
+        <Tile>
+            <MoviePoster src={exampleMoviePoster} alt="movie poster" />
+            <div>
+                <MovieTileHeader>Mulan</MovieTileHeader>
+                <MovieTileYear>2020</MovieTileYear>
+                <GenresList>
+                    <Genres>Action</Genres>
+                    <Genres>Adventure</Genres>
+                    <Genres>Drama</Genres>
+                </GenresList>
+                <MovieRating>
+                    <StyledStarIcon />
+                    <Rating>7,8</Rating>
+                    <Votes>35 votes</Votes>
+                </MovieRating>
+            </div>
+        </Tile>
+    );
+};
 
 export default MovieTile;
