8d7e9f11e2f72a8da63f2395d8f241e5a405ee3f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-21 01:58:22 +0100

    add styled-components for MovieTileHeader, MovieTileYear, GenresList, and Genres

--- src/features/movies/MovieTile/index.js
+++ src/features/movies/MovieTile/index.js
@@ -1,9 +1,18 @@
 import exampleMoviePoster from "../../../images/exampleMoviePoster.jpg";
-import { Tile, MoviePoster } from "./styled";
+import { Tile, MoviePoster, MovieTileHeader, MovieTileYear, GenresList, Genres } from "./styled";
 
 const MovieTile = () => (
     <Tile>
         <MoviePoster src={exampleMoviePoster} alt="movie poster" />
+        <div>
+            <MovieTileHeader>Mulan</MovieTileHeader>
+            <MovieTileYear>2020</MovieTileYear>
+            <GenresList>
+                <Genres>Action</Genres>
+                <Genres>Adventure</Genres>
+                <Genres>Drama</Genres>
+            </GenresList>
+        </div>
     </Tile>
 );
 
--- src/features/movies/MovieTile/styled.js
+++ src/features/movies/MovieTile/styled.js
@@ -15,3 +15,31 @@ export const MoviePoster = styled.img`
     width: 100%;
     border-radius: 5px;
 `;
+
+export const MovieTileHeader = styled.h2`
+    font-size: 22px;
+    font-weight: 500;
+    margin: 16px 0 8px 0;
+`;
+
+export const MovieTileYear = styled.span`
+    color: #7E839A;
+`;
+
+export const GenresList = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+    padding: 0;
+    margin: 8px 0 39px 0;
+    list-style: none;
+`;
+
+export const Genres = styled.li`
+    font-size: 14px;
+    background-color: #E4E6F0;
+    padding: 8px 16px;
+    border-radius: 5px;
+`;
+
+
