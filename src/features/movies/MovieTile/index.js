e002d95230688fb0402bdb57071dc396b16a7462
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-22 02:31:20 +0100

    render MovieTileHeader, MovieTileYear, Rating and Votes from API data

--- src/features/movies/MovieTile/index.js
+++ src/features/movies/MovieTile/index.js
@@ -30,6 +30,15 @@ const MovieTile = () => {
             {movies.map((movie) => (
                 <Tile key={movie.id}>
                     <MoviePoster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                    <div>
+                        <MovieTileHeader>{movie.title}</MovieTileHeader>
+                        <MovieTileYear>{(movie.release_date).slice(0, 4)}</MovieTileYear>
+                        <MovieRating>
+                            <StyledStarIcon />
+                            <Rating>{(movie.vote_average).toFixed(1)}</Rating>
+                            <Votes>{movie.vote_count} votes</Votes>
+                        </MovieRating>
+                    </div>
                 </Tile>
             ))}
         </>
