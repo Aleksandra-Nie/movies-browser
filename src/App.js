243bc72376a21311c4e5c91cfb10cbb683509721
monwil999 <monikawilkpr@gmail.com>
2025-01-23 20:55:10 +0100

    ThemeProvider, theme and Header added to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import MoviesPage from "./features/movies/MoviesPage";
 import PeoplePage from "./features/people/PeoplePage";
 import PersonPage from "./features/people/PersonPage";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
+import { Header } from "./common/Navigation/Header";
+import MoviesContainer from "./features/movies/MoviesContainer";
 
 function App() {
   return (
-    <>
+
+    <ThemeProvider theme={theme}>
+      <Header />
+      <MoviesContainer />
       <HashRouter>
         <MoviesPage />
         <Routes>
@@ -14,7 +21,7 @@ function App() {
           <Route path="/person/:id" element={<PersonPage />} />
         </Routes>
       </HashRouter>
-    </>
+  </ThemeProvider>
   );
 }
 
