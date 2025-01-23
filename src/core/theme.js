8ad41591b6969dd623f04c803d2f57097ccb91d6
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-23 02:59:31 +0100

    add new colors and breakpoints to theme.js file

--- src/core/theme.js
+++ src/core/theme.js
@@ -3,9 +3,18 @@ export const theme = {
     black: "#18181B",
     white: "#FFFFFF",
     mystic: "#E4E6F0",
+    candlelight: "#FCD420",
     mineShaft: "#333333",
     pattensBlue: "#D6E4FF",
     waterloo: "#7E839A",
+    softBlue: "#BAC7D580",
     scienceBlue: "#0044CC",
+    woodsmoke: "#18181B",
+  },
+
+  breakpoints: {
+    mobile: 480,
+    tablet: 767,
+    desktop: 1440,
   },
 };
