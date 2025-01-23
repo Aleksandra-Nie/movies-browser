fcecef0cc44f8eb700a7c1892b21ebe28c7ca4a7
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-23 02:31:04 +0100

    add new colors to theme.js

--- src/core/theme.js
+++ src/core/theme.js
@@ -2,5 +2,10 @@ export const theme = {
   color: {
     black: "#18181B",
     white: "#FFFFFF",
+    mystic: "#E4E6F0",
+    mineShaft: "#333333",
+    pattensBlue: "#D6E4FF",
+    waterloo: "#7E839A",
+    scienceBlue: "#0044CC",
   },
 };
