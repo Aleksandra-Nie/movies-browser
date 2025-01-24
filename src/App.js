22cba3579f456521fbe9e95477aa21b9f2e97e83
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-25 00:06:22 +0100

    add HashRouter and Routes with Route to PersonPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MoviesPage from "./features/movies/MoviesPage";
 import PeoplePage from "./features/people/PeoplePage";
+import PersonPage from "./features/people/PersonPage";
+
 
 function App() {
   return (
     <>
-      <MoviesPage />
-      <PeoplePage />
+      {/* <MoviesPage /> */}
+      <HashRouter>
+        <PeoplePage />
+        <Routes>
+          <Route path="/person/:id" element={<PersonPage />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
