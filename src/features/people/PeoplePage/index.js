4a0091c33cf813f76b5297b7bd81e9697eea81fe
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-24 01:24:12 +0100

    add Srollbar component to PeoplePage component

--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -1,9 +1,19 @@
+import { fetchPeople } from "../fetchPeopleData";
+import { selectCurrentPage, selectTotalPages, setCurrentPage, setPeople } from "../peopleSlice";
 import PeopleContainer from "../PeopleContainer";
+import Scrollbar from "../../Scrollbar";
 import { Wrapper } from "../../../common/index";
 
 const PeoplePage = () => (
     <Wrapper>
         <PeopleContainer />
+        <Scrollbar
+            fetchData={fetchPeople}
+            setData={setPeople}
+            setCurrentPage={setCurrentPage}
+            selectCurrentPage={selectCurrentPage}
+            selectTotalPages={selectTotalPages}
+        />
     </Wrapper>
 );
 
