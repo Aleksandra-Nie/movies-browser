14a8bb938edc5453542f25e5066c1860046369ed
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-25 00:48:10 +0100

    colors, shadow and breakpoint added to theme.js

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,6 +1,24 @@
 export const theme = {
   color: {
     black: "#18181B",
+    blackSmoke: "#000000",
+    blackPearl: "#03121F",
+    stormGray: "#74788B",
+    waterloo: "#7E839A",
+    mystic: "#E4E6F0",
+    candleLight: "#FCD420",
+    whisper: "#F7F5FA",
     white: "#FFFFFF",
+  },
+  shadow: {
+    tile: "0px 4px 12px 0px #BAC7D580",
+  },
+  breakpoint: {
+    mobileXS: 319,
+    mobileS: 510,
+    mobileMax: 767,
+    tabletVerticalMax: 991,
+    tabletHorizontalMax: 1199,
+    laptopMax: 1400,
   },
 };
