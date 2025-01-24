0d2795a2ff3c20faf72b4a712998a2dae00637c1
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-25 00:44:16 +0100

    GlobalStyle, theme and ThemeProvider added to main index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './core/theme';
+import { GlobalStyle } from './core/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
