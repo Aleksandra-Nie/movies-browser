5b0b90da5a2c34a55ca5005d9db382d0d72fd0c6
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-25 19:34:16 +0100

    add sharedStyles.js file

--- src/features/movies/MoviesContainer/styled.js
+++ src/features/movies/MoviesContainer/styled.js
@@ -5,28 +5,3 @@ export const StyledMoviesContainer = styled.div`
         margin: 16px;
     };
 `;
-
-export const Header = styled.h2`
-    font-size: 36px;
-    font-weight: 600;
-    margin-top: 56px;
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px){
-        font-size: 18px;
-    };
-`;
-
-export const MovieTilesContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 24px;
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.desktop}px){
-        grid-template-columns: 1fr 1fr 1fr;
-        gap: 16px;
-    };
-
-    @media(max-width: ${({ theme }) => theme.breakpoints.tablet}px){
-        grid-template-columns: 1fr;
-    };
-`;
