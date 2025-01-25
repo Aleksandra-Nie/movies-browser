ac4501e369172b392d3049ce33fddba3fca6092f
monwil999 <monikawilkpr@gmail.com>
2025-01-25 21:06:05 +0100

    SearchInput added to Nav

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,12 +1,15 @@
 import { Background, Wrapper} from "./styled.js";
 import { Header } from "./Header";
+import  SearchInput from "./Search";
 
 export const Navigation = () => {
   return (
     <Background>
       <Wrapper>
         <Header />
+        <SearchInput />
       </Wrapper>
     </Background>
   );
 };
+export default Navigation;
