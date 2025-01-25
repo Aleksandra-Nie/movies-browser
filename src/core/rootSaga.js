442064b575cea54481001d9217f9cda4472e59c8
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-25 02:51:38 +0100

    add watchFetchPeopleDetailsOnPageChange saga to rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,10 +1,11 @@
 import { all } from "redux-saga/effects";
 import { watchFetchMoviesData } from "../features/movies/moviesSaga";
-import { watchFetchPeopleData } from "../features/people/peopleSaga";
+import { watchFetchPeopleData, watchFetchPeopleDetailsOnPageChange } from "../features/people/peopleSaga";
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviesData(),
         watchFetchPeopleData(),
+        watchFetchPeopleDetailsOnPageChange(),
     ]);
 };
