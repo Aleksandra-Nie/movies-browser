2d3d04d796d73deb4c3cb38f8db3c7755ac21fbc
monwil999 <monikawilkpr@gmail.com>
2025-01-25 21:06:26 +0100

    Navigation added, Header removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./core/theme";
-import { Header } from "./common/Navigation/Header";
+import Navigation from "./common/Navigation";
 import MoviesContainer from "./features/movies/MoviesContainer";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
+      <Navigation />
       <MoviesContainer />
     </ThemeProvider>
   );
