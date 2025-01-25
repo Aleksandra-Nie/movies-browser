157507422373a8aa58ffa0df5f4825d44a81a3fc
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-25 02:50:43 +0100

    add watchFetchPeopleDetailsOnPageChange and fetchPeopleDetailsOnPageChangeHandler sagas

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,11 +1,32 @@
-import { put, call, takeLatest } from "redux-saga/effects";
-import { fetchPeopleFailure, fetchPeopleRequest, fetchPeopleSuccess } from "./peopleSlice";
-import { fetchPeople } from "./fetchPeopleData";
+import { put, call, takeLatest, all, select } from "redux-saga/effects";
+import { fetchPeopleFailure, fetchPeopleRequest, fetchPeopleSuccess, selectCurrentPage, setCurrentPage, setPersonDetails } from "./peopleSlice";
+import { fetchPeople, fetchPersonDetails } from "./fetchPeopleData";
 
 function* fetchPeopleDataHandler() {
     try {
         const people = yield call(fetchPeople);
         yield put(fetchPeopleSuccess(people));
+
+        const personDetails = yield all(people.results.map(person =>
+            call(fetchPersonDetails, person.id),
+        ));
+        yield put(setPersonDetails(personDetails));
+
+    } catch (error) {
+        yield put(fetchPeopleFailure(error));
+    };
+};
+
+function* fetchPeopleDetailsOnPageChangeHandler() {
+    try {
+        const currentPage = yield select(selectCurrentPage);
+        const people = yield call(fetchPeople, currentPage);
+
+        const personDetails = yield all(people.results.map(person =>
+            call(fetchPersonDetails, person.id),
+        ));
+        yield put(setPersonDetails(personDetails));
+
     } catch (error) {
         yield put(fetchPeopleFailure(error));
     };
@@ -14,3 +35,7 @@ function* fetchPeopleDataHandler() {
 export function* watchFetchPeopleData() {
     yield takeLatest(fetchPeopleRequest.type, fetchPeopleDataHandler);
 };
+
+export function* watchFetchPeopleDetailsOnPageChange() {
+    yield takeLatest(setCurrentPage.type, fetchPeopleDetailsOnPageChangeHandler);
+}
