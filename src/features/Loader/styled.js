e1e7fd7a912e8c84be5eee4edb5a29b26f72c587
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-27 21:10:32 +0100

    add media-queries for responsive design

--- src/features/Loader/styled.js
+++ src/features/Loader/styled.js
@@ -5,6 +5,17 @@ export const Title = styled.p`
     font-weight: 600;
     color: ${({ theme }) => theme.color.woodsmoke};
     margin: 56px 0 120px 0;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tablet}px){
+        font-size: 24px;
+        margin: 35px 0 70px 0;
+    };
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+        font-size: 14px;
+        font-weight: 500;
+        margin: 24px 0 24px 0;
+    };
 `;
 
 const rotation = keyframes`
@@ -29,5 +40,19 @@ export const StyledLoader = styled.span`
     border-bottom-color: ${({ theme }) => theme.color.black};
     border-radius: 50%;
     animation: ${rotation} 1s linear infinite;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tablet}px){
+        width: 55px;
+        height: 55px;
+        border: 8px solid ${({ theme }) => theme.color.snuff};
+        border-bottom-color: ${({ theme }) => theme.color.black};
+    };
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+        width: 35px;
+        height: 35px;
+        border: 5px solid ${({ theme }) => theme.color.snuff};
+        border-bottom-color: ${({ theme }) => theme.color.black};
+    };
 `;
 
