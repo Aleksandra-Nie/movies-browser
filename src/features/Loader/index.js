24e7ca7ae3ae718902bf087ef6bc5b65815b47c2
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-27 20:37:51 +0100

    add Loader component

---
+++ src/features/Loader/index.js
@@ -0,0 +1,12 @@
+import { Wrapper } from "../../common";
+
+const Loader = () => (
+    <Wrapper>
+        <h2>Search results for "Mulan"</h2>
+        <div>
+            <div></div>
+        </div>
+    </Wrapper>
+);
+
+export default Loader;
