8310b8316acb0db373ad7e7f292d79ee27497e25
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-30 23:22:24 +0100

    corrections in CSS properties for Search component

--- src/common/Navigation/Search/styled.js
+++ src/common/Navigation/Search/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { ReactComponent as SearchIcon } from "../../../icons/Search.svg";
 
 export const Wrapper = styled.div`
-  width: 432px;
+  max-width: 432px;
+  width: 100%;
   height: 48px;
   display: flex;
   gap: 16px;
@@ -10,19 +11,10 @@ export const Wrapper = styled.div`
   background: ${({ theme }) => theme.color.white};
   border: 1px solid ${({ theme }) => theme.color.mystic};
   border-radius: 50px;
-  padding: 0 24px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVertialMax}) {
-    width: 100%;
+ 
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVerticalMax}px) {
+    max-width: 100%;
     height: 44px;
-    margin-top: 20px;
-    padding: 0 20px;
-  }
-
-  @media (${({ theme }) => theme.breakpoints.mobileMax}) {
-    max-width: 288px;
-    height: 44px;
-    margin-top: 30px;
     padding: 0 20px;
   }
 `;
@@ -33,9 +25,11 @@ export const SearchInput = styled.input`
   font-size: 16px;
   line-height: 24px;
   border: none;
-  border-radius: 30px;
+  border-radius: 33px;
+  width: 100%;
+  font-family: "Poppins", sans-serif;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVertialMax}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletVertialMax}px) {
     font-size: 14.5px;
     line-height: 20px;
   }
@@ -44,12 +38,15 @@ export const SearchInput = styled.input`
     max-width: 288px;
     font-size: 13px;
     line-height: 17px;
+    margin-top: 24px;
   }
 `;
 
 export const StyledSearchIcon = styled(SearchIcon)`
   width: 24px;
   height: 24px;
+  padding-left: 24px;
+  color: ${({ theme }) => theme.color.waterloo};
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletVertialMax}px) {
     width: 22px;
