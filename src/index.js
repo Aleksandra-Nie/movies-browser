e74bd799414c84dfb05466cebaf6811818314181
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-30 23:16:55 +0100

    GlobalStyle and theme added to main index.js to apply proper colors&font

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './core/theme';
-import { GlobalStyles } from './core/globalStyles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
+import { GlobalStyles } from "./core/globalStyles";
+import { GlobalStyle } from "./core/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <GlobalStyles />
-        <App />
       </Provider>
+      <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
