61a33c78d471f42cfe94a597478b170a9587bc09
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-01-30 23:16:04 +0100

    color waterloo added to theme

--- src/core/theme.js
+++ src/core/theme.js
@@ -3,14 +3,15 @@ export const theme = {
     black: "#18181B",
     white: "#FFFFFF",
     mystic: "#E4E6F0",
+    waterloo: "#7E839A",
   },
 
- breakpoints: {
+  breakpoints: {
     mobileXS: 319,
     mobileS: 510,
     mobileMax: 767,
     tabletVerticalMax: 991,
     tabletHorizontalMax: 1199,
     laptopMax: 1400,
-    },
+  },
 };
