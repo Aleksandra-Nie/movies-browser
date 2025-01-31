6cf4589cde4c38e04e6d22ad177d1342190ccc74
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-31 23:40:29 +0100

    add Scrollbar for query when searchParams exist

--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -1,18 +1,31 @@
-import { fetchPeople } from "../fetchPeopleData";
-import { selectTotalPages, setPeople } from "../peopleSlice";
+import { useSelector } from "react-redux";
+import { fetchPeople, fetchPeopleByQuery } from "../fetchPeopleData";
+import { selectSearchQuery, selectTotalPages, setPeople } from "../peopleSlice";
 import PeopleContainer from "../PeopleContainer";
 import Scrollbar from "../../Scrollbar";
 import { Wrapper } from "../../../common/index";
 
-const PeoplePage = () => (
-    <Wrapper>
-        <PeopleContainer />
-        <Scrollbar
-            fetchData={fetchPeople}
-            setData={setPeople}
-            selectTotalPages={selectTotalPages}
-        />
-    </Wrapper>
-);
+const PeoplePage = () => {
+    const searchQuery = useSelector(selectSearchQuery);
+
+    return (
+        <Wrapper>
+            <PeopleContainer />
+            {searchQuery ? (
+                <Scrollbar
+                    fetchData={fetchPeopleByQuery}
+                    setData={setPeople}
+                    selectTotalPages={selectTotalPages}
+                />
+            ) : (
+                <Scrollbar
+                    fetchData={fetchPeople}
+                    setData={setPeople}
+                    selectTotalPages={selectTotalPages}
+                />
+            )}
+        </Wrapper>
+    );
+};
 
 export default PeoplePage;
