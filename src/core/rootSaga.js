88ab6a60d58b1f8589fa14cb87cdb2741f52c3e8
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-31 23:22:21 +0100

    add watchFetchPeopleByQuery saga to rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,11 +1,16 @@
 import { all } from "redux-saga/effects";
 import { watchFetchMoviesData } from "../features/movies/moviesSaga";
-import { watchFetchPeopleData, watchFetchPeopleDataOnPageChange } from "../features/people/peopleSaga";
+import {
+    watchFetchPeopleByQuery,
+    watchFetchPeopleData,
+    watchFetchPeopleDataOnPageChange
+} from "../features/people/peopleSaga";
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviesData(),
         watchFetchPeopleData(),
         watchFetchPeopleDataOnPageChange(),
+        watchFetchPeopleByQuery(),
     ]);
 };
