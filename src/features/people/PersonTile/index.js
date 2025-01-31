132ab4a1155e64ba19946e89b3b7576526a8991d
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-01-31 22:22:17 +0100

    add page argument to fetchPeopleRequest action for proper data fetching after refresh

--- src/features/people/PersonTile/index.js
+++ src/features/people/PersonTile/index.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { fetchPeopleRequest, selectPeople } from "../../people/peopleSlice";
 import { Tile, PersonName, PersonPhoto } from "../personStyles";
 import { Link } from "./styled";
 
 const PersonTile = () => {
+    const location = useLocation();
     const dispatch = useDispatch();
 
+    const queryParams = new URLSearchParams(location.search);
+    const currentPage = parseInt(queryParams.get("page")) || 1;
+
     useEffect(() => {
-        dispatch(fetchPeopleRequest());
+        dispatch(fetchPeopleRequest({ page: currentPage }));
     }, []);
 
     const peopleData = useSelector(selectPeople);
