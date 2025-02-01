296e278ab2d0bae03f4160e9c610319f4fc495a8
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-01 01:10:29 +0100

    add search functionality with debounce for input field

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,11 +1,51 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import searchQueryParamName from "../../../features/searchQueryParamName";
 import { Wrapper, SearchInput, StyledSearchIcon } from "./styled";
 
 const Search = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+
+  useEffect(() => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    const timeoutId = setTimeout(() => {
+      const searchParams = new URLSearchParams(location.search);
+
+      if (searchQuery.trim() === "") {
+        searchParams.delete(searchQueryParamName);
+      } else {
+        searchParams.set(searchQueryParamName, searchQuery);
+      }
+
+      navigate(`${location.pathname}?${searchParams.toString()}`);
+    }, 500);
+
+    setDebounceTimeout(timeoutId);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
+  const onInputChange = ({ target }) => {
+    setSearchQuery(target.value);
+  };
+
   return (
     <Wrapper>
       <StyledSearchIcon />
-      <SearchInput type="text" placeholder="Search for movies..." />
+      <SearchInput
+        type="text"
+        placeholder="Search for people..."
+        value={searchQuery || ""}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
