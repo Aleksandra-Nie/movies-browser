35268c0384cbc2d2e32cf703f0d1944f0e037d59
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-01 02:15:30 +0100

    change Header text for search results and add loader during loading

--- src/features/people/PeopleContainer/index.js
+++ src/features/people/PeopleContainer/index.js
@@ -1,13 +1,25 @@
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import PersonTile from "../PersonTile";
+import Loader from "../../Loader";
 import { StyledPeopleContainer, Header, PeopleTilesContainer } from "./styled";
+import { selectLoading } from "../peopleSlice";
+import searchQueryParamName from "../../searchQueryParamName";
 
-const PeopleContainer = () => (
-    <StyledPeopleContainer>
-        <Header>Popular people</Header>
-        <PeopleTilesContainer>
-            <PersonTile />
-        </PeopleTilesContainer>
-    </StyledPeopleContainer>
-);
+const PeopleContainer = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
+    const loading = useSelector(selectLoading);
+
+    return (
+        <StyledPeopleContainer>
+            {loading && <Loader />}
+            <Header>{searchParams !== null ? `Search results for "${searchParams}"` : `Popular people`}</Header>
+            <PeopleTilesContainer>
+                <PersonTile />
+            </PeopleTilesContainer>
+        </StyledPeopleContainer>
+    );
+};
 
 export default PeopleContainer;
