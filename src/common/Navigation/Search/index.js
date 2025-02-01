ecd05a00a1e9e431221276abfb78d95833b1b85e
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-01 02:14:22 +0100

    reset search input and searchQuery when query parameter does not exist

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
+import { resetSearchQuery } from "../../../features/people/peopleSlice";
 import searchQueryParamName from "../../../features/searchQueryParamName";
 import { Wrapper, SearchInput, StyledSearchIcon } from "./styled";
 
 const Search = () => {
+  const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
   const query = new URLSearchParams(location.search).get(searchQueryParamName);
 
   const [searchQuery, setSearchQuery] = useState(query || "");
   const [debounceTimeout, setDebounceTimeout] = useState(null);
+
+  useEffect(() => {
+    if (!query) {
+      setSearchQuery("");
+      dispatch(resetSearchQuery());
+    }
+  }, [query]);
 
   useEffect(() => {
     if (debounceTimeout) {
