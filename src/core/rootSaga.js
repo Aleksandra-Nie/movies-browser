85f9a5f32c394c1d115df7241084c5f5602262d9
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-02 20:10:18 +0100

    remove unnecessary saga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -3,14 +3,12 @@ import { watchFetchMoviesData } from "../features/movies/moviesSaga";
 import {
     watchFetchPeopleByQuery,
     watchFetchPeopleData,
-    watchFetchPeopleDataOnPageChange
 } from "../features/people/peopleSaga";
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviesData(),
         watchFetchPeopleData(),
-        watchFetchPeopleDataOnPageChange(),
         watchFetchPeopleByQuery(),
     ]);
 };
--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,5 +1,5 @@
-import { put, call, takeLatest, all, select } from "redux-saga/effects";
-import { fetchPeople, fetchPeopleByQuery, fetchPeopleCredits, fetchPersonDetails } from "./fetchPeopleData";
+import { put, call, takeLatest } from "redux-saga/effects";
+import { fetchPeople, fetchPeopleByQuery } from "./fetchPeopleData";
 import {
     fetchPeopleByQueryFailure,
     fetchPeopleByQueryRequest,
@@ -7,10 +7,6 @@ import {
     fetchPeopleFailure,
     fetchPeopleRequest,
     fetchPeopleSuccess,
-    selectCurrentPage,
-    setCurrentPage,
-    setPeopleCredits,
-    setPersonDetails,
 } from "./peopleSlice";
 
 function* fetchPeopleDataHandler(action) {
@@ -24,40 +20,11 @@ function* fetchPeopleDataHandler(action) {
     };
 };
 
-function* fetchPeopleDataOnPageChangeHandler() {
-    try {
-        const currentPage = yield select(selectCurrentPage);
-        const people = yield call(fetchPeople, currentPage);
-
-        const personDetails = yield all(people.results.map(person =>
-            call(fetchPersonDetails, person.id),
-        ));
-        const personCredits = yield all(people.results.map(person =>
-            call(fetchPeopleCredits, person.id),
-        ));
-        yield put(setPersonDetails(personDetails));
-        yield put(setPeopleCredits(personCredits));
-
-    } catch (error) {
-        yield put(fetchPeopleFailure(error));
-    };
-};
-
 function* fetchPeopleByQueryHandler(action) {
     try {
         const searchParams = action.payload;
         const peopleByQuery = yield call(fetchPeopleByQuery, searchParams);
-
         yield put(fetchPeopleByQuerySuccess(peopleByQuery));
-
-        const personDetails = yield all(peopleByQuery.results.map(person =>
-            call(fetchPersonDetails, person.id),
-        ));
-        const personCredits = yield all(peopleByQuery.results.map(person =>
-            call(fetchPeopleCredits, person.id),
-        ));
-        yield put(setPersonDetails(personDetails));
-        yield put(setPeopleCredits(personCredits));
 
     } catch (error) {
         yield put(fetchPeopleByQueryFailure(error));
@@ -70,8 +37,4 @@ export function* watchFetchPeopleByQuery() {
 
 export function* watchFetchPeopleData() {
     yield takeLatest(fetchPeopleRequest.type, fetchPeopleDataHandler);
-};
-
-export function* watchFetchPeopleDataOnPageChange() {
-    yield takeLatest(setCurrentPage.type, fetchPeopleDataOnPageChangeHandler);
 };
