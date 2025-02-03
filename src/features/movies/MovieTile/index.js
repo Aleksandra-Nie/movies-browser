ca1cc6dd4b5d7e3d1302966e87666317c2ef1bd6
monwil999 <monikawilkpr@gmail.com>
2025-02-03 22:51:25 +0100

    Link from React Router Dom added

--- src/features/movies/MovieTile/index.js
+++ src/features/movies/MovieTile/index.js
@@ -1,51 +1,57 @@
+import { Link } from "react-router-dom";
 import {
-    Tile,
-    MoviePoster,
-    MovieTileHeader,
-    MovieTileYear,
-    GenresList,
-    Genres,
-    MovieRating,
-    StyledStarIcon,
-    Rating,
-    Votes,
+  Tile,
+  MoviePoster,
+  MovieTileHeader,
+  MovieTileYear,
+  GenresList,
+  Genres,
+  MovieRating,
+  StyledStarIcon,
+  Rating,
+  Votes,
 } from "./styled";
 import cameraIcon from "../../../images/camera.png";
 
 const MovieTile = ({
-    id,
-    title,
-    poster_path,
-    release_date,
-    genre_ids,
-    genresMap,
-    vote_average,
-    vote_count,
+  id,
+  title,
+  poster_path,
+  release_date,
+  genre_ids,
+  genresMap,
+  vote_average,
+  vote_count,
 }) => {
-    return (
-        <Tile key={id}>
-            <MoviePoster
-                src={poster_path ? `https://image.tmdb.org/t/p/w400${poster_path}` : cameraIcon}
-                alt={`${title} poster`}
-            />
-            <div>
-                <MovieTileHeader>{title}</MovieTileHeader>
-                <MovieTileYear>{release_date?.slice(0, 4)}</MovieTileYear>
-                <GenresList>
-                    {genre_ids.map((id) => (
-                        <Genres key={id}>
-                            {genresMap[id] || "Unknown"}
-                        </Genres>
-                    ))}
-                </GenresList>
-                <MovieRating>
-                    <StyledStarIcon />
-                    <Rating>{vote_average.toFixed(1)}</Rating>
-                    <Votes>{vote_count} votes</Votes>
-                </MovieRating>
-            </div>
-        </Tile>
-    );
+  return (
+    <Link to={`/movies/${id}`}>
+      {" "}
+      <Tile key={id}>
+        <MoviePoster
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w400${poster_path}`
+              : cameraIcon
+          }
+          alt={`${title} poster`}
+        />
+        <div>
+          <MovieTileHeader>{title}</MovieTileHeader>
+          <MovieTileYear>{release_date?.slice(0, 4)}</MovieTileYear>
+          <GenresList>
+            {genre_ids.map((id) => (
+              <Genres key={id}>{genresMap[id] || "Unknown"}</Genres>
+            ))}
+          </GenresList>
+          <MovieRating>
+            <StyledStarIcon />
+            <Rating>{vote_average.toFixed(1)}</Rating>
+            <Votes>{vote_count} votes</Votes>
+          </MovieRating>
+        </div>
+      </Tile>
+    </Link>
+  );
 };
 
 export default MovieTile;
