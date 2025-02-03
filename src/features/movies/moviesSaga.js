253ce6203ee4b4ffcc4bc82f82bbc034ac02b945
monwil999 <monikawilkpr@gmail.com>
2025-02-03 22:38:02 +0100

    fetchMoviesFailure,   fetchMoviesRequest,   fetchMoviesSuccess imported from movieSlice

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,19 +1,22 @@
 import { call, put, takeLatest } from "redux-saga/effects";
-import { fetchMoviesFailure, fetchMoviesRequest, fetchMoviesSuccess } from "./moviesSlice";
+import {
+  fetchMoviesFailure,
+  fetchMoviesRequest,
+  fetchMoviesSuccess,
+} from "./moviesSlice";
 import { fetchGenres, fetchMovies } from "./fetchMoviesData";
 
 function* fetchMoviesDataHandler(action) {
-    try {
-        const page = action.payload.page;
-        const movies = yield call(fetchMovies, page);
-        const genres = yield call(fetchGenres);
-        yield put(fetchMoviesSuccess({ movies, genres, page }));
-
-    } catch (error) {
-        yield put(fetchMoviesFailure(error));
-    };
-};
+  try {
+    const page = action.payload.page;
+    const movies = yield call(fetchMovies, page);
+    const genres = yield call(fetchGenres);
+    yield put(fetchMoviesSuccess({ movies, genres, page }));
+  } catch (error) {
+    yield put(fetchMoviesFailure(error));
+  }
+}
 
 export function* watchFetchMoviesData() {
-    yield takeLatest(fetchMoviesRequest.type, fetchMoviesDataHandler);
-};
+  yield takeLatest(fetchMoviesRequest.type, fetchMoviesDataHandler);
+}
