16e249c5340bfd7471c09c1c9669b2278afb9092
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-04 23:47:08 +0100

    add margin left to title

--- src/features/NoResults/styled.js
+++ src/features/NoResults/styled.js
@@ -10,7 +10,7 @@ export const Container = styled.div`
 
 export const Title = styled.h2`
     align-self: flex-start;
-    margin: 56px 0 0 0;
+    margin: 56px 0 0 56px;
     color: ${({ theme }) => theme.color.woodsmoke};
     font-size: 36px;
     font-weight: 600;
