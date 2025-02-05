8bc28cb284c5657fc862d579c5a284b6cbad8bd1
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-05 20:51:04 +0100

    add search functionality for movies

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,18 +1,33 @@
-import { fetchMovies } from "../fetchMoviesData";
+import { useLocation } from "react-router-dom";
+import { fetchMovies, fetchMoviesByQuery } from "../fetchMoviesData";
 import { selectTotalPages, setMovies } from "../moviesSlice";
 import MoviesContainer from "../MoviesContainer";
 import Scrollbar from "../../Scrollbar";
 import { Wrapper } from "../../../common/index";
+import searchQueryParamName from "../../searchQueryParamName";
 
-const MoviesPage = () => (
-  <Wrapper>
-    <MoviesContainer />
-    <Scrollbar
-      fetchData={fetchMovies}
-      setData={setMovies}
-      selectTotalPages={selectTotalPages}
-    />
-  </Wrapper>
-);
+const MoviesPage = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  return (
+    <Wrapper>
+      <MoviesContainer />
+      {searchParams ? (
+        <Scrollbar
+          fetchData={fetchMoviesByQuery}
+          setData={setMovies}
+          selectTotalPages={selectTotalPages}
+        />
+      ) : (
+        <Scrollbar
+          fetchData={fetchMovies}
+          setData={setMovies}
+          selectTotalPages={selectTotalPages}
+        />
+      )}
+    </Wrapper>
+  );
+};
 
 export default MoviesPage;
