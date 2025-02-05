4f92bf56beeda698022859cd19ac06c21ef30f9c
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-05 19:47:02 +0100

    add sagas for fetchMoviesByQuery

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,10 +1,13 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import {
+  fetchMoviesByQueryFailure,
+  fetchMoviesByQueryRequest,
+  fetchMoviesByQuerySuccess,
   fetchMoviesFailure,
   fetchMoviesRequest,
   fetchMoviesSuccess,
 } from "./moviesSlice";
-import { fetchGenres, fetchMovies } from "./fetchMoviesData";
+import { fetchGenres, fetchMovies, fetchMoviesByQuery } from "./fetchMoviesData";
 
 function* fetchMoviesDataHandler(action) {
   try {
@@ -14,9 +17,23 @@ function* fetchMoviesDataHandler(action) {
     yield put(fetchMoviesSuccess({ movies, genres, page }));
   } catch (error) {
     yield put(fetchMoviesFailure(error));
-  }
+  };
 }
+
+function* fetchMoviesByQueryHandler(action) {
+  try {
+    const searchParams = action.payload;
+    const moviesByQuery = yield call(fetchMoviesByQuery, searchParams);
+    yield put(fetchMoviesByQuerySuccess(moviesByQuery));
+  } catch (error) {
+    yield put(fetchMoviesByQueryFailure(error));
+  };
+};
+
+export function* watchFetchMoviesByQuery() {
+  yield takeLatest(fetchMoviesByQueryRequest.type, fetchMoviesByQueryHandler);
+};
 
 export function* watchFetchMoviesData() {
   yield takeLatest(fetchMoviesRequest.type, fetchMoviesDataHandler);
-}
+};
