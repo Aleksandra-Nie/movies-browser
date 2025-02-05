38bcb7717ea179e3c63a11cd871e7527b52346ef
Tymoteusz Wojcik <timmywojcik@gmail.com>
2025-02-05 21:22:16 +0100

    add NoResults component for empty search results and show PopularMovies on search reset

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,14 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { fetchMovies, fetchMoviesByQuery } from "../fetchMoviesData";
-import { selectTotalPages, setMovies } from "../moviesSlice";
+import { selectMovies, selectTotalPages, setMovies, setTotalPages } from "../moviesSlice";
 import MoviesContainer from "../MoviesContainer";
 import Scrollbar from "../../Scrollbar";
+import NoResults from "../../NoResults";
 import { Wrapper } from "../../../common/index";
 import searchQueryParamName from "../../searchQueryParamName";
 
 const MoviesPage = () => {
+  const dispatch = useDispatch();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  const moviesData = useSelector(selectMovies);
+  const movies = moviesData.results;
+
+  if (!searchParams) {
+    dispatch(setTotalPages(500));
+  }
+
+  if (!searchParams) {
+    return (
+      <Wrapper>
+        <MoviesContainer />
+        <Scrollbar
+          fetchData={fetchMovies}
+          setData={setMovies}
+          selectTotalPages={selectTotalPages}
+        />
+      </Wrapper>
+    );
+  }
+
+  if (Array.isArray(movies) && movies.length === 0) {
+    return (<NoResults />);
+  }
 
   return (
     <Wrapper>
