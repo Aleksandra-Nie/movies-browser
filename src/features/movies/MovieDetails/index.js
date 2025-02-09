48c8c8d5a4bb3608e4c221dd3b26c18313e1e0e7
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-02-09 17:29:39 +0100

    No data available communication added to MoviesDetails

--- src/features/movies/MovieDetails/index.js
+++ src/features/movies/MovieDetails/index.js
@@ -85,120 +85,119 @@ const MovieDetails = () => {
     return <Loader />;
   } else if (error) {
     return <ErrorScreen />;
-  } else if (!movieDetails) {
-    return <div>Loading movie details...</div>;
-  } else if (!movieCredits) {
-    return <div>Loading movie credits...</div>;
+  } else if (!movieDetails || !movieCredits) {
+    return <div>No data available</div>;
+  } else {
+
+    return (
+      <>
+        <IntroductionLayer
+          title={movieDetails.title}
+          rating={movieDetails.vote_average}
+          votes={movieDetails.vote_count}
+          backdropPath={movieDetails.backdrop_path}
+        />
+
+        <Container>
+          <MovieTile $movieDetails>
+            <Poster
+              $movieDetails
+              src={
+                movieDetails.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+                  : exampleMoviePoster
+              }
+              alt={movieDetails.title}
+            />
+            <MovieSection>
+              <Header>{movieDetails.title}</Header>
+              <Year>
+                {movieDetails.release_date
+                  ? movieDetails.release_date.split("-")[0]
+                  : "Unknown"}
+              </Year>
+              <DetailsContainer>
+                <LineContainer>
+                  <Caption>Production:</Caption>
+                  <Text>
+                    {movieDetails.production_countries
+                      ?.map((country) => country.name)
+                      .join(", ") || "Unknown"}
+                  </Text>
+                </LineContainer>
+                <LineContainer>
+                  <Caption>Release date:</Caption>
+                  <Text>
+                    {movieDetails.release_date
+                      ? formatDateToLocale(movieDetails.release_date)
+                      : "Unknown"}
+                  </Text>
+                </LineContainer>
+              </DetailsContainer>
+              <GenresList>
+                {movieDetails.genres?.map((genre) => (
+                  <Genres key={genre.id}>{genre.name}</Genres>
+                ))}
+              </GenresList>
+              <MovieRating>
+                <StyledStarIcon />
+                <Rating>{movieDetails.vote_average?.toFixed(1) || "N/A"}</Rating>
+                <MaxRating>/ 10</MaxRating>
+                <Votes>{movieDetails.vote_count} votes</Votes>
+              </MovieRating>
+            </MovieSection>
+            <Description>
+              {movieDetails.overview || "No description available."}
+            </Description>
+          </MovieTile>
+        </Container>
+
+        {movieCredits && (
+          <>
+            <Section>
+              <Title>Cast ({movieCredits.cast?.length || 0})</Title>
+              <PeopleContainer>
+                {movieCredits.cast?.map((actor) => (
+                  <PeopleTile key={actor.id}>
+                    <Image
+                      src={
+                        actor.profile_path
+                          ? `https://image.tmdb.org/t/p/w185${actor.profile_path}`
+                          : exampleCast
+                      }
+                      alt={actor.name}
+                    />
+                    <PeopleName>{actor.name}</PeopleName>
+                    <PeopleRole>{actor.character}</PeopleRole>
+                  </PeopleTile>
+                ))}
+              </PeopleContainer>
+            </Section>
+
+            <Section>
+              <Title>Crew ({movieCredits.crew?.length || 0})</Title>
+              <PeopleContainer>
+                {movieCredits.crew?.map((crew) => (
+                  <PeopleTile key={crew.id}>
+                    <Image
+                      src={
+                        crew.profile_path
+                          ? `https://image.tmdb.org/t/p/w185${crew.profile_path}`
+                          : exampleCast
+                      }
+                      alt={crew.name}
+                    />
+                    <PeopleName>{crew.name}</PeopleName>
+                    <PeopleRole>{crew.job}</PeopleRole>
+                  </PeopleTile>
+                ))}
+              </PeopleContainer>
+            </Section>
+          </>
+        )}
+      </>
+    );
   }
-
-  return (
-    <>
-      <IntroductionLayer
-        title={movieDetails.title}
-        rating={movieDetails.vote_average}
-        votes={movieDetails.vote_count}
-        backdropPath={movieDetails.backdrop_path}
-      />
-
-      <Container>
-        <MovieTile $movieDetails>
-          <Poster
-            $movieDetails
-            src={
-              movieDetails.poster_path
-                ? `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
-                : exampleMoviePoster
-            }
-            alt={movieDetails.title}
-          />
-          <MovieSection>
-            <Header>{movieDetails.title}</Header>
-            <Year>
-              {movieDetails.release_date
-                ? movieDetails.release_date.split("-")[0]
-                : "Unknown"}
-            </Year>
-            <DetailsContainer>
-              <LineContainer>
-                <Caption>Production:</Caption>
-                <Text>
-                  {movieDetails.production_countries
-                    ?.map((country) => country.name)
-                    .join(", ") || "Unknown"}
-                </Text>
-              </LineContainer>
-              <LineContainer>
-                <Caption>Release date:</Caption>
-                <Text>
-                  {movieDetails.release_date
-                    ? formatDateToLocale(movieDetails.release_date)
-                    : "Unknown"}
-                </Text>
-              </LineContainer>
-            </DetailsContainer>
-            <GenresList>
-              {movieDetails.genres?.map((genre) => (
-                <Genres key={genre.id}>{genre.name}</Genres>
-              ))}
-            </GenresList>
-            <MovieRating>
-              <StyledStarIcon />
-              <Rating>{movieDetails.vote_average?.toFixed(1) || "N/A"}</Rating>
-              <MaxRating>/ 10</MaxRating>
-              <Votes>{movieDetails.vote_count} votes</Votes>
-            </MovieRating>
-          </MovieSection>
-          <Description>
-            {movieDetails.overview || "No description available."}
-          </Description>
-        </MovieTile>
-      </Container>
-
-      {movieCredits && (
-        <>
-          <Section>
-            <Title>Cast ({movieCredits.cast?.length || 0})</Title>
-            <PeopleContainer>
-              {movieCredits.cast?.map((actor) => (
-                <PeopleTile key={actor.id}>
-                  <Image
-                    src={
-                      actor.profile_path
-                        ? `https://image.tmdb.org/t/p/w185${actor.profile_path}`
-                        : exampleCast
-                    }
-                    alt={actor.name}
-                  />
-                  <PeopleName>{actor.name}</PeopleName>
-                  <PeopleRole>{actor.character}</PeopleRole>
-                </PeopleTile>
-              ))}
-            </PeopleContainer>
-          </Section>
-
-          <Section>
-            <Title>Crew ({movieCredits.crew?.length || 0})</Title>
-            <PeopleContainer>
-              {movieCredits.crew?.map((crew) => (
-                <PeopleTile key={crew.id}>
-                  <Image
-                    src={
-                      crew.profile_path
-                        ? `https://image.tmdb.org/t/p/w185${crew.profile_path}`
-                        : exampleCast
-                    }
-                    alt={crew.name}
-                  />
-                  <PeopleName>{crew.name}</PeopleName>
-                  <PeopleRole>{crew.job}</PeopleRole>
-                </PeopleTile>
-              ))}
-            </PeopleContainer>
-          </Section>
-        </>
-      )}
-    </>
-  );
 };
 
 export default MovieDetails;
