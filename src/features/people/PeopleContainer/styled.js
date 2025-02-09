ef67b5e221ea618082a2825ec60fabc8fdf0ce90
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-02-09 17:37:20 +0100

    Add fade-in animation to PeopleContainer component with StyledPeopleContainer

--- src/features/people/PeopleContainer/styled.js
+++ src/features/people/PeopleContainer/styled.js
@@ -1,7 +1,18 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 export const StyledPeopleContainer = styled.div`
     margin-top: 56px;
+    opacity: 0;
+    animation: ${fadeIn} 1s ease-in-out forwards;
 `;
 
 export const Header = styled.h2`
