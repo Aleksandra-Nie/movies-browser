24d9b93304bd96ad8d5ff21d8ac73396daf5ce46
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-02-09 17:35:48 +0100

    Implement dynamic display for search results count and popular people based on search query

--- src/features/people/PeopleContainer/index.js
+++ src/features/people/PeopleContainer/index.js
@@ -3,18 +3,26 @@ import { useLocation } from "react-router-dom";
 import PersonTile from "../PersonTile";
 import Loader from "../../Loader";
 import { StyledPeopleContainer, Header, PeopleTilesContainer } from "./styled";
-import { selectLoading } from "../peopleSlice";
+import { selectPeople, selectLoading } from "../peopleSlice";
 import searchQueryParamName from "../../searchQueryParamName";
 
 const PeopleContainer = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
+    const peopleData = useSelector(selectPeople);
+    const people = peopleData.results;
     const loading = useSelector(selectLoading);
+
+    const resultCount = people ? people.length : 0;
 
     return (
         <StyledPeopleContainer>
             {loading && <Loader />}
-            <Header>{searchParams !== null ? `Search results for "${searchParams}"` : `Popular people`}</Header>
+            <Header>
+                {searchParams
+                    ? `Search results for "${searchParams}" (${resultCount})`
+                    : `Popular people`}
+            </Header>
             <PeopleTilesContainer>
                 <PersonTile />
             </PeopleTilesContainer>
@@ -23,3 +31,4 @@ const PeopleContainer = () => {
 };
 
 export default PeopleContainer;
+
