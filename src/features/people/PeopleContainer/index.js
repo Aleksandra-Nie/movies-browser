84abb7f08542cf43d93bbe44a188db6922ebe33d
monwil999 <monikawilkpr@gmail.com>
2025-02-14 23:21:08 +0100

    totalResults and   selectTotalResults added

--- src/features/people/PeopleContainer/index.js
+++ src/features/people/PeopleContainer/index.js
@@ -3,32 +3,38 @@ import { useLocation } from "react-router-dom";
 import PersonTile from "../PersonTile";
 import Loader from "../../Loader";
 import { StyledPeopleContainer, Header, PeopleTilesContainer } from "./styled";
-import { selectPeople, selectLoading } from "../peopleSlice";
+import {
+  selectPeople,
+  selectLoading,
+  selectTotalResults,
+} from "../peopleSlice";
 import searchQueryParamName from "../../searchQueryParamName";
 
 const PeopleContainer = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
-    const peopleData = useSelector(selectPeople);
-    const people = peopleData?.results;
-    const loading = useSelector(selectLoading);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search).get(
+    searchQueryParamName
+  );
+  const peopleData = useSelector(selectPeople);
+  const people = peopleData?.results || [];
+  const loading = useSelector(selectLoading);
+  const totalResults = useSelector(selectTotalResults);
 
-    const resultCount = people ? people.length : 0;
-
-    return (
-        <StyledPeopleContainer>
-            {loading && <Loader />}
-            <Header>
-                {searchParams
-                    ? `Search results for "${searchParams}" (${resultCount})`
-                    : `Popular people`}
-            </Header>
-            <PeopleTilesContainer>
-                <PersonTile />
-            </PeopleTilesContainer>
-        </StyledPeopleContainer>
-    );
+  return (
+    <StyledPeopleContainer>
+      {loading && <Loader />}
+      <Header>
+        {searchParams
+          ? `Search results for "${searchParams}" (${totalResults})`
+          : `Popular people`}
+      </Header>
+      <PeopleTilesContainer>
+        {people.map((person) => (
+          <PersonTile key={person.id} {...person} />
+        ))}
+      </PeopleTilesContainer>
+    </StyledPeopleContainer>
+  );
 };
 
 export default PeopleContainer;
-
