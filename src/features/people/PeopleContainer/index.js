11cad3a1fa1ac772fe95c7335881eebac091f10c
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2025-02-17 15:21:10 +0100

    query and resultCount adjustment so that it is updated every time when query changes

--- src/features/people/PeopleContainer/index.js
+++ src/features/people/PeopleContainer/index.js
@@ -1,34 +1,63 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import PersonTile from "../PersonTile";
+import { useQueryCurrentPage } from "../../useQueryCurrentPage";
+import {
+    fetchPeopleByQueryRequest,
+    fetchPeopleRequest,
+    selectPeople,
+    selectLoading,
+} from "../peopleSlice";
 import Loader from "../../Loader";
-import { StyledPeopleContainer, Header, PeopleTilesContainer } from "./styled";
-import { selectPeople, selectLoading } from "../peopleSlice";
+import NoResults from "../../NoResults";
+import { PeopleTilesContainer } from "./styled";
+import { Header } from "../../sharedStyles";
 import searchQueryParamName from "../../searchQueryParamName";
+import PersonTile from "../PersonTile";
+import { FadeInWrapper } from "../../../common/index";
 
 const PeopleContainer = () => {
+    const dispatch = useDispatch();
     const location = useLocation();
-    const searchParams = new URLSearchParams(location.search).get(searchQueryParamName);
+    const currentPage = useQueryCurrentPage();
+
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get(searchQueryParamName);
+
+    useEffect(() => {
+        if (!query) {
+            dispatch(fetchPeopleRequest({ page: currentPage }));
+        } else {
+            dispatch(fetchPeopleByQueryRequest(query, { page: currentPage }));
+        }
+    }, [query, currentPage, dispatch]);
+
     const peopleData = useSelector(selectPeople);
-    const people = peopleData?.results;
+    const people = peopleData?.results || [];
     const loading = useSelector(selectLoading);
 
-    const resultCount = people ? people.length : 0;
+    const resultCount = query && peopleData ? peopleData.total_results : people.length;
 
     return (
-        <StyledPeopleContainer>
-            {loading && <Loader />}
-            <Header>
-                {searchParams
-                    ? `Search results for "${searchParams}" (${resultCount})`
-                    : `Popular people`}
-            </Header>
-            <PeopleTilesContainer>
-                <PersonTile />
-            </PeopleTilesContainer>
-        </StyledPeopleContainer>
+        <FadeInWrapper>
+            {!loading && (query ? (people.length > 0 && <Header>Search results for "{query}" ({resultCount})</Header>) : <Header>Popular people</Header>)}
+
+            {loading ? (
+                <>
+                    {query && <Header>Search results for "{query}"</Header>}
+                    <Loader />
+                </>
+            ) : people.length === 0 && query ? (
+                <NoResults />
+            ) : (
+                <PeopleTilesContainer>
+                    {people.map((person) => (
+                        <PersonTile key={person.id} person={person} />
+                    ))}
+                </PeopleTilesContainer>
+            )}
+        </FadeInWrapper >
     );
 };
 
 export default PeopleContainer;
-
